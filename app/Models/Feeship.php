433660ccf682ebc271a3f4efa97dbc54851c284f
hieuchance2018 <hieuchance2018@gmail.com>
2025-10-18 15:06:41 +0700

    New Updated

--- app/Models/Feeship.php
+++ app/Models/Feeship.php
@@ -18,14 +18,14 @@ class Feeship extends Model
 
 	public function city()
 	{
-		return $this->belongsTo('App\City', 'fee_matp');
+		return $this->belongsTo('App\Models\City', 'fee_matp');
 	}
 	public function province()
 	{
-		return $this->belongsTo('App\Province', 'fee_maqh');
+		return $this->belongsTo('App\Models\Province', 'fee_maqh');
 	}
 	public function wards()
 	{
-		return $this->belongsTo('App\Wards', 'fee_xaid');
+		return $this->belongsTo('App\Models\Wards', 'fee_xaid');
 	}
 }
